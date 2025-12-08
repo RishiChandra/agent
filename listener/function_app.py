32c67f940e7e18733b7805f8615bc03072c34495
RishiChandra <rishi2000chandra@gmail.com>
2025-12-07 20:21:20 -0800

    Add Defer Logic to Listener Function App

--- listener/function_app.py
+++ listener/function_app.py
@@ -4,40 +4,51 @@ import json
 import logging
 import sys
 from pathlib import Path
+import os
 
+from datetime import datetime, timedelta, UTC
 from session_management_utils import get_session
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
 app = func.FunctionApp()
-
+connection_string = os.getenv("AZURE_SERVICEBUS_CONNECTION_STRING")
 @app.service_bus_queue_trigger(arg_name="msg", 
                                 queue_name="q1",
                                 connection="AzureWebJobsServiceBus")
 def QueueWorker(msg: func.ServiceBusMessage):
-    logging.info('Python HTTP trigger function processed a request.')
     print('Python HTTP trigger function processed a request.')
     body = msg.get_body().decode("utf-8")
-    logging.info(f"Received message: {body}")
+    print(f"Received message: {body}")
     
     # Process the message body
     try:
-        message_data = json.loads(body)
-        logging.info(f"Parsed message data: {message_data}")
-        print(f"Parsed message data: {message_data}")
-
         user_id = "2ba330c0-a999-46f8-ba2c-855880bdcf5b"
                 
         try:
            session = get_session(user_id)
            if session:
-                logging.info(f"FOUND SESSION FOR USER {user_id}")
-                print(f"FOUND SESSION FOR USER {user_id}")
+                print(f"FOUND SESSION FOR USER {user_id} Session: {session}")
+                if session["is_active"] == True:
+                    print(f"SESSION IS ACTIVE FOR USER {user_id}")
+                    print(f"Connection string: {connection_string}")
+                    scheduled_time = datetime.now(UTC) + timedelta(minutes=1)
+                    try:
+                        with ServiceBusClient.from_connection_string(connection_string) as client:
+                            with client.get_queue_sender("q1") as sender:
+                                message = ServiceBusMessage(body)
+                                sender.schedule_messages(message, scheduled_time)
+                                print(f"✅ Message deffered for {scheduled_time.strftime('%Y-%m-%d %H:%M:%S UTC')}")
+                    except Exception as e:
+                        print(f"Error: {e}")
+                        sys.exit(1)
+                else:
+                    # JASON ADD MOCK WS FUNCTION HERE
+                    print(f"SESSION IS INACTIVE FOR USER {user_id}")
+
            else:
-                logging.info(f"COULD NOT FIND SESSION FOR USER {user_id}")
                 print(f"COULD NOT FIND SESSION FOR USER {user_id}")
         except Exception as e:
-            logging.error(f"Error querying tasks table: {e}")
             print(f"Error querying tasks table: {e}")
             
     except json.JSONDecodeError:
-        logging.info(f"Message is not JSON, raw body: {body}")
         print(f"Message is not JSON, raw body: {body}")
